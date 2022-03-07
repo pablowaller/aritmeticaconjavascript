35fcd8c046e86787a1414b1d3fd6587ec79afef0
pablowaller <76492840+pablowaller@users.noreply.github.com>
2022-03-07 06:41:49 -0300

    Add files via upload

--- js/calculos.js
+++ js/calculos.js
@@ -1,6 +1,13 @@
 /// Sumatorio
 
-function sumatorio() {
+function sumatorioSigma() {
+ 
+    const num1 = document.getElementById("number1").value
+
+    if (!num1) {
+        alert("Debe ingresar un valor.");
+        return false
+    }
 
     let sum = 0;
 
@@ -8,11 +15,21 @@ function sumatorio() {
         sum += 3 * n;
         return sum
     }
+
+    
+    $('#res1').html
 }
 
 /// Productorio
 
-function productorio() {
+function productorioPi() {
+
+    const num2 = document.getElementById("number2").value
+
+    if (!num2) {
+        alert("Debe ingresar un valor.");
+        return false
+    }
 
     let prod = 1;
 
@@ -20,15 +37,18 @@ function productorio() {
         prod *= 2 * n;
         console.log(prod)
     }
+
+    
+    $('#res2').html
 }
 
 /// Sucesion de Fibonacci
 
 function sucesionDeFibonacci() {
 
-    const num = document.getElementById("number").value
+    const num3 = document.getElementById("number3").value
 
-    if (!num) {
+    if (!num3) {
         alert("Debe ingresar un valor.");
         return false
     }
@@ -44,15 +64,14 @@ function sucesionDeFibonacci() {
             return fib(n - 1) + fib(n - 2);
         }
 
-        $('#res3').html(`<h2 style='color: #064A86;'>La posición ${num} se trata del número ${fib(num)} de la sucesión.</h2>`)
+        $('#res3').html(`<h2 style='color: #064A86;'>La posición ${num3} se trata del número ${fib(num3)} de la sucesión.</h2>`)
     }
 };
 
 // Algoritmo para la funcionalidad del input type number en dispositivos móviles
 
-if(/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream)
-{
+if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
     var inputs = document.querySelectorAll('input[type="number"]');
-    for(var i = inputs.length; i--;)
+    for (var i = inputs.length; i--;)
         inputs[i].setAttribute('pattern', '\\d*');
 }
